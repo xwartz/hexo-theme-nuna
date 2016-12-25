a9b722882810bbbc912fd515c6a752e32ba32e27
xwartz <stddup@gmail.com>
2016-12-25 17:00:10 +0800

    Fix loading display

--- source/script/loading.js
+++ source/script/loading.js
@@ -1,14 +1,12 @@
 ;(function () {
-  window.addEventListener('load', function (event) {
-    var loadingBar = document.getElementsByClassName('loading-bar')[0]
-    window.pageAccelerator && window.pageAccelerator({
-      beforeLoading: function () {
-        loadingBar.classList.add('isloading')
-      },
-      afterLoading: function () {
-        loadingBar.classList.remove('isloading')
-      }
-    })
+  var loadingBar = document.getElementsByClassName('loading-bar')[0]
+  window.pageAccelerator && window.pageAccelerator({
+    beforeLoading: function () {
+      loadingBar.classList.add('isloading')
+    },
+    afterLoading: function () {
+      loadingBar.classList.remove('isloading')
+    }
   })
 }())
 
